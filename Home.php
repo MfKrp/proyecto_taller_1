b94e961e8e6acd29af50e8a208c2adba182bc4e6
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-04-19 17:26:11 -0300

    Update Home.php

--- Home.php
+++ Home.php
@@ -1,15 +1,33 @@
 <?php
-
 namespace App\Controllers;
-
 class Home extends BaseController
 {
-    public function index()
+    
+    public function quienes_somos() 
     {
-        return view('principal_proyect/principal.html')
-        .view('principal_proyect/nav.html')
-        .view('principal_proyect/quienes_somos.html')
-        .view('principal_proyect/seccion_principal.html')
-        .view('principal_proyect/footer.html');
+        $data['titulo'] = 'quienes_somos';
+        echo view('principal_proyect/principal.php', $data);
+        echo view('principal_proyect/nav.php');
+        echo view('principal_proyect/quienes_somos.php');
+        echo view('principal_proyect/footer.php');
     }
+
+    public function principal()
+    {
+        $data['titulo'] = 'principal';
+        echo view('principal_proyect/principal.php');
+        echo view('principal_proyect/nav.php');
+        echo view('principal_proyect/seccion_principal.php');
+        echo view('principal_proyect/footer.php');
+    }
+    
+    //FUNCION PRINCIPAL DE EMERGENCIA SI NO FUNCIONA EL ENRUTAMIENTO MODULAR
+    /*public function index()
+    {
+        return view('principal_proyect/principal.php')
+        .view('principal_proyect/nav.php')
+        .view('principal_proyect/quienes_somos.php')
+        .view('principal_proyect/seccion_principal.php')
+        .view('principal_proyect/footer.php');
+    }*/
 }
