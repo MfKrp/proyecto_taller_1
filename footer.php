74d038e3b0816cd1fbc9cadcd00f83d02757c6b5
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-04-21 18:16:32 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!--FOOTER QUE CONTIENE INFORMACION ADICIONAL ACERCA DE CONSULTAS E INFORMACION DE CONTACTOS Y ETC-->
-<div class="container-fluid bg-dark pt-1">
-    <footer class="py-1">
+<div class="container-fluid bg-dark py-1" id="footer-div">
+    <footer class="" id="footer">
         <ul class="nav justify-content-center border-bottom pb-3 mb-3">
             <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Consultas</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-light">Terminos de uso</a></li>
