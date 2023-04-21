ef863133fb5724ae7893056658eb5c86016d41c3
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-04-21 17:18:12 -0300

    Update seccion_principal.php

--- seccion_principal.php
+++ seccion_principal.php
@@ -24,26 +24,161 @@
                 <!--Seccion de juegos-->
                 <h4 class="mt-4 p-2 text-center rounded bg-dark" id="GAMES-NAVIGATION-SECTION">Juegos disponibles</h4>
                 <div class="row row-cols-auto catalogue-section">
+                    
+                    <!--Age of empires 2: Definitive Edition-->
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\age_of_empires_2.jpg" alt="age of empires 2 definite edition" class="product-image rounded">
-                        <p class="product-features">Las caracteristicas de este producto son</p>
-                        <p>Criticas al producto</p>
+                        
+                        <hr>
+                        
+                        <p class="product-name rounded">Age of Empires 2: Definitive edition</p>
+
+                        <div class="price-div rounded">
+                            <p>$5000</p>
+                        </div>
+
+                        <div class="buy-button rounded">
+                            <!--Simbolo de carrito-->
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+
+                            <!--Fin de simbolo de carrito-->
+
+                            <span>Comprar</span>
+                        </div>
+
+                        <hr>
+
+                        <div class="product-features rounded">
+                            <p>
+                                Las caracteristicas del producto son las siguientes:
+                            </p>
+                            <ul>
+                                <li>Estilo isometrico</li>
+                                <li>Estrategia en tiempo real</li>
+                                <li>Medieval</li>
+                            </ul>
+                        </div>
+                        
+                        <div class="product-critics bg-warning text-dark rounded">
+                            <p>
+                                Criticas al producto:
+                            </p>
+                            <ul>
+                                <li>Curva de aprendizaje empinada</li>
+                                <li>Vista por sectores</li>
+                                <li>Inversion de tiempo para mejorar</li>
+                            </ul>
+                        </div>
                     </div>
+                    
+                    <!--Bioshock infinite-->
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\bioshock_infinite.png" alt="bioshock infinite" class="product-image rounded">
-                        <p class="product-features text-center">Las caracteristicas de este producto son</p>
-                        <p>Criticas al producto</p>
+                        
+                        <hr>
+                        
+                        <p class="product-name rounded">Bioshock Infinite</p>
+
+                        <div class="price-div rounded">
+                            <p>$3350</p>
+                        </div>
+
+                        <div class="buy-button rounded">
+                            <!--Simbolo de carrito-->
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+
+                            <!--Fin de simbolo de carrito-->
+
+                            <span>Comprar</span>
+                        </div>
+
+                        <hr>
+
+                        <div class="product-features rounded">
+                            <p>
+                                Las caracteristicas del producto son las siguientes:
+                            </p>
+                            <ul>
+                                <li>Shooter en primera persona</li>
+                                <li>Accion y Fantasia</li>
+                                <li>Contiene violencia (especificacion T)</li>
+                            </ul>
+                        </div>
+                        
+                        <div class="product-critics bg-warning text-dark rounded">
+                            <p>
+                                Criticas al producto:
+                            </p>
+                            <ul>
+                                <li>Tiende a la monotonia</li>
+                                <li>Unicamente posee modo historia</li>
+                            </ul>
+                        </div>
                     </div>
+                    
+                    <!--Hollow Knight-->
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\hollow_knight.jpg" alt="hollow knight" class="product-image rounded">
-                        <p class="product-features">Las caracteristicas de este producto son</p>
-                        <p>Criticas al producto</p>
+                        <hr>
+                        
+                        <p class="product-name rounded">Hollow Knight</p>
+
+                        <div class="price-div rounded">
+                            <p>$1500</p>
+                        </div>
+
+                        <div class="buy-button rounded">
+                            <!--Simbolo de carrito-->
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+
+                            <!--Fin de simbolo de carrito-->
+
+                            <span>Comprar</span>
+                        </div>
+
+                        <hr>
+
+                        <div class="product-features rounded">
+                            <p>
+                                Las caracteristicas del producto son las siguientes:
+                            </p>
+                            <ul>
+                                <li>Metroidvania</li>
+                                <li>Fantasia</li>
+                                <li>Artistico</li>
+                                <li>Exploración</li>
+                            </ul>
+                        </div>
+                        
+                        <div class="product-critics bg-warning text-dark rounded">
+                            <p>
+                                Criticas al producto:
+                            </p>
+                            <ul>
+                                <li>Dificultad elevada</li>
+                                <li>Exploracion excesiva</li>
+                                <li>Una vez finalizada la historia principal y los dlc, el juego se queda sin contenido</li>
+                            </ul>
+                        </div>
                     </div>
+                    
+                    <!--Hotline Miami-->
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\hotline_miami.jpg" alt="hotline miami" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
+                    <!--Stardew Valley-->
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\stardew_valley.jpg" alt="stardew valley" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
@@ -54,26 +189,31 @@
                 <!--Seccion de componentes-->
                 <h4 class="mt-4 p-2 text-center rounded bg-dark" id="COMPONENTS-NAVIGATION-SECTION">Componentes disponibles</h4>
                 <div class="row row-cols-auto catalogue-section">
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\geforce_rtx_3070.png" alt="geforce rtx 3070 edition" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\kingston_fury_ddr4_8gb.png" alt="kingston fury ddr4 3200mhz" class="product-image rounded">
                         <p class="product-features text-center">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\asus_a520_mk.png" alt="Asus a520m-k" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\ryzen_5_5600g.png" alt="Ryzen 5 5600g" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\sentey_steel_nitro_power_550.png" alt="Sentey steel nitro power 550w" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
@@ -84,17 +224,20 @@
                 <!--Seccion de perifericos-->
                 <h4 class="mt-4 p-2 text-center rounded bg-dark" id="PERIFERICS-NAVIGATION-SECTION">Perifericos disponibles</h4>
                 <div class="row row-cols-auto catalogue-section">
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\razer_blackwidow.png
                         " alt="Razer Blackwidow" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\hyperx_alloy_origins.png" alt="Hyperx alloy origins" class="product-image rounded">
                         <p class="product-features text-center">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\redragon_kumara.png" alt="Redragon kumara" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
@@ -104,7 +247,9 @@
                         <img src="assets\img\redragon_lamia_white.png" alt="Redragon lamia white" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
+                    
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\logitech_g203.png" alt="Logitech g203" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
@@ -116,26 +261,31 @@
                 <!--Seccion de consolas-->
                 <h4 class="mt-4 p-2 text-center rounded bg-dark" id="CONSOLES-NAVIGATION-SECTION">Consolas disponibles</h4>
                 <div class="row row-cols-auto catalogue-section">
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\play_5.png" alt="Playstation 5" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\xbox_seriesx.png" alt="Xbox Series x" class="product-image rounded">
                         <p class="product-features text-center">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\xbox_one.png" alt="Xbox one" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\play_2.png" alt="Playstation 2" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\nintendo_switch.png" alt="Nintendo switch" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
@@ -146,21 +296,25 @@
                 <!--Seccion de servicios-->
                 <h4 class="mt-4 p-2 text-center rounded bg-dark"  id="SERVICES-NAVIGATION-SECTION">Servicios disponibles</h4>
                 <div class="row row-cols-auto catalogue-section">
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\cybersecurity.png" alt="cybersecurity" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\import.png" alt="Device importation" class="product-image rounded">
                         <p class="product-features text-center">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\web_design.png" alt="Web design" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
                         <p>Criticas al producto</p>
                     </div>
+                    
                     <div class="col text-center ITEM-BOX bg-dark">
                         <img src="assets\img\pc_repair_logo.png" alt="Device fixing" class="product-image rounded">
                         <p class="product-features">Las caracteristicas de este producto son</p>
