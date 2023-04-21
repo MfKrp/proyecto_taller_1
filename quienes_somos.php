fbdf67b7b530e4df23cc30472dbc33a379ca405a
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-04-21 18:15:48 +0100

    Update and rename quienes_somos.html to quienes_somos.php

---
+++ quienes_somos.php
@@ -0,0 +1,21 @@
+<!--QUIENES SOMOS-->
+<div class="d-flex flex-column text-center text-light" id="WHO-WE-ARE">
+    <h1>
+        Breve sinopsis
+    </h1>
+    <p>
+        Hola! Como te encuentras, espero estes teniendo un lindo dia, si estas en nuestra pagina es porque has oido de nosotros por algun medio o has recibido de parte de alguien una recomendacion hacia nosotros, pues dejame darte una explicacion acerca de quienes somos y en que nos enfocamos, Steel Technology, un emprendimiento que nacio para hacer llegar de forma accesible y eficiente productos de caracter tecnologico tanto fisico como digital.
+    </p>
+    <h3>
+        ¿Como surgimos?
+    </h3>
+    <p>
+        Nacimos del interes de poder hacer llegar a cualquier parte el recurso mas importante que puede haber; informacion, ademas de brindar entretenimiento digital, con el objetivo de reducir la ignorancia y aumentar las posibilidades de que la gente tenga informacion y herramientas para, si ellos desean, mejorar sus chances de oportunidades mejores para aumentar su calidad de vida sin que sea necesario hacer sacrificios muy grandes por dicha formacion y acceso
+    </p>
+    <h3>
+        ¿Como esta compuesto nuestro equipo?
+    </h3>
+    <p>
+        Nuestro equipo esta conformado por diseñadores UI y UX, programadores especializados tanto en FRONTEND como BACKEND para el desarrollo de esta aplicacion, en caso de considerar que tienes las habilidades para formar parte de nuestro equipo puedes enviar un mensaje a cualquiera de nuestros metodos de contacto especificados en la seccion de <a href="#" class="text-info text-decoration-none">Contactos</a>
+    </p>
+</div>
